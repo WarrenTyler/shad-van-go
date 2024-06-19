0e2cac77ad49ea5fe10977637552a189b4fa7bfb
WarrenT <warrentyler1024@gmail.com>
2024-06-19 06:02:53 +0100

    feat: Add Home and About pages with React Router setup
    - Create pages/Home.jsx and pages/About.jsx components
    - Update tailwind.config.js to handle JavaScript files (.js, .jsx)
    - Import BrowserRouter, Routes, Route from react-router-dom in App.jsx
    - Add <BrowserRouter> wrapper in App.jsx
    - Define <Routes> and <Route> components in App.jsx
    - Set up routes for Home and About pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 const App = () => {
   return (
-    <div>App Start Project</div>
-  )
-}
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <section className="mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </section>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <h1 className="text-2xl">Home, start here</h1>;
+}
+export default Home
