3d4c3e1e935b12ec6d9507e234439d3e806caa64
WarrenT <warrentyler1024@gmail.com>
2024-06-21 04:43:50 +0100

    feat: implement Home page component
    - Add hero section with background image
    - Include page content and styling
    - Add a link to navigate to the vans page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1 className="text-2xl">Home, start here</h1>;
+  return (
+    // <div className="hero-image">
+    <div className="home-container">
+      <h1>You got the travel plans, we got the travel vans.</h1>
+      <p>
+        Add adventure to your life by joining the #vanlife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <Link to="vans">Find your van</Link>
+    </div>
+  );
 }
 export default Home
