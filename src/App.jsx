36b73a62af7f290af16c5389cf6d5152c1971665
WarrenT <warrentyler1024@gmail.com>
2024-06-21 04:40:39 +0100

    refactor: update App component with header and navigation
    - Add header section with site logo and navigation links
    - Use Link component from react-router-dom for navigation links
    - Wrap Routes with BrowserRouter and set basename
    - Remove unnecessary section element
    - Add catch-all route for navigation to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <section className="mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </section>
+    <BrowserRouter basename="/shad-van-go">
+      <header>
+        <Link className="site-logo" to="/">
+          Home
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </BrowserRouter>
   );
 };
