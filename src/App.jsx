60c88ff357419cebbc88ec65b84d41399147a4ec
WarrenT <warrentyler1024@gmail.com>
2024-06-22 09:08:26 +0100

    feat: add route for VanDetail component
    - Import VanDetail component in App.jsx (line 7)
      - VanDetail component to display details of a specific van
    
    - Add Route for "/vans/:id" path in App.jsx (line 26)
      - Route renders VanDetail component
      - :id parameter to match specific van ID
      - Allows navigating to individual van details page
    
    - Enable routing to VanDetail component based on van ID
    - Supports displaying details of a selected van

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import "./App.css";
 import "../server.js";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
