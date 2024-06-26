7dfbc367c85c8109f9755a2b7b69d3711d00d16c
WarrenT <warrentyler1024@gmail.com>
2024-06-26 22:10:32 +0100

    feat: add Header component with navigation links
    - Import Link and NavLink from react-router-dom
    - Create Header functional component
    - Render header element with site logo (Link to home)
    - Add navigation links using NavLink
      - Host, About, Vans
    - Apply active class to current NavLink
    - Export Header component for use in the application

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link className="site-logo" to="/">
+        Home
+      </Link>
+      <nav>
+        <NavLink to="/host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
+        <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : null}>About</NavLink>
+        <NavLink to="/vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
+      </nav>
+    </header>
+  );
+}
+export default Header
