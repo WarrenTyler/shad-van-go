fa4b79afdea554ffc3d78e7e2187c771ece68999
WarrenT <warrentyler1024@gmail.com>
2024-06-26 22:22:29 +0100

    refactor: restructure routing and add HostLayout
    - Remove Header component from App.jsx
    - Import Layout, HostLayout, and host pages components
    - Wrap routes with Layout component
      - Nest Home, About, Vans, and VanDetail routes under Layout
    - Add HostLayout route with nested routes
      - Dashboard as index route
      - Reviews and Income routes
    - Update import paths for Vans and VanDetail components
    - Remove unnecessary Link imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import "./App.css";
 import "../server.js";
-import Vans from "./pages/Vans";
-import VanDetail from "./pages/VanDetail";
+import Vans from "./pages/vans/Vans";
+import VanDetail from "./pages/vans/VanDetail";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/host/Dashboard";
+import Income from "./pages/host/Income";
+import Reviews from "./pages/host/Reviews";
+import HostLayout from "./components/HostLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          Home
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="income" element={<Income />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
